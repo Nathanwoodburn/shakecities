3ba951fa42ea7d60f86c7db383860777cc3ad79d
Nathan Woodburn <github@nathan.woodburn.au>
2023-11-08 22:51:39 +1100

    feat: Remove scripts from render

--- sites/website.py
+++ sites/website.py
@@ -1,8 +1,17 @@
 from flask import Flask, make_response, redirect, render_template_string, request, jsonify, render_template, send_from_directory
-
+from bs4 import BeautifulSoup
 
 def render(data):
     if data == "":
         return "No data found for this domain"
     
-    return render_template_string(data)
+    try:
+        soup = BeautifulSoup(data, 'html.parser')
+        for script in soup.find_all('script'):
+            script.extract()
+        modified_data = str(soup)
+        return render_template_string(modified_data)
+
+
+    except Exception as e:
+        return "<h1>Invalid HTML</h1><br>" + str(e)
